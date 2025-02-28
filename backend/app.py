bdf8ac7618c6909ec9253737add8b63df35e2ca4
Edgar <ceggarr199@gmail.com>
2025-02-28 00:04:03 -0600

    tests de asignacion y lectura de usuarios confirmados

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,21 @@
-from flask import Flask,redirect,url_for,request,jsonify,Response
+from flask import Flask, jsonify,redirect,url_for
+from db import registrar_cliente, ver_clientes
 
+app = Flask(__name__)
 
-app=Flask(__name__)
+@app.route("/")
+def index():
+    return jsonify(ver_clientes())
 
+@app.route("/registrar")
+def registrar():
+    resp=registrar_cliente({
+        "id": 5567,
+        "nombre": "Gael gonzales torres",
+        "contacto": "6352330967",
+        "domicilio": "Av.tierra santa 220, 31522"
+    })
+    return jsonify(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- backend/db.py
+++ backend/db.py
@@ -1,16 +1,51 @@
+from flask import jsonify
 import pymysql
 import pymysql.cursors
 import config
 from colorama import Fore
 
+conn = pymysql.connect(**config.DB_CONFIG, cursorclass=pymysql.cursors.DictCursor)
+cursor = conn.cursor()
+
 def conectar():
     try:
-        conexion=pymysql.connect(**config.DB_CONFIG,cursorclass=pymysql.cursors.DictCursor)
-        print(Fore.GREEN +"✅ Conexion Exitosa")
-        return conexion
+        print(Fore.GREEN + "✅ Conexion Exitosa")
+        print(Fore.WHITE +"")
+        return conn
     except Exception as e:
         print(Fore.RED + f"❌ Error de conexion: {e}")
+        print(Fore.WHITE +"")
         return None
-        
+
+def ver_clientes():
+    try:
+        cursor.execute("SELECT * FROM CLIENTES")
+        data = cursor.fetchall()
+        print(f"Tipo de 'data': {type(data)}")
+        print(f"Contenido de 'data': {data}")
+        return data
+    except Exception as e:
+        print(Fore.RED + f"❌ Error al obtener clientes: {e}")
+        print(Fore.WHITE +"")
+        return jsonify({"Error": str(e)})
+
+def registrar_cliente(data):
+    try:
+        print(data)
+        cursor.execute(
+            "INSERT INTO CLIENTES(id, nombre, contacto, domicilio) values(%s, %s, %s, %s)", 
+            (data['id'], data['nombre'], data['contacto'], data['domicilio'])
+        )
+        print(Fore.GREEN + "✅ Cliente agregado con éxito!!!")
+        print(Fore.WHITE +"")
+        conn.commit()
+        return {"mensaje": "usuario agregado con éxito"}
+    except pymysql.MySQLError as e:
+        print(Fore.RED + f"❌ Error de base de datos: {str(e)}")
+        print(Fore.WHITE +"")    
+        return {"error": str(e)}
+    except Exception as e:
+        print(Fore.RED + f"❌ Error desconocido: {str(e)}")
+        print(Fore.WHITE +"")
+        return {"error": str(e)}
     
-conectar()
